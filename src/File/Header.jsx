9c7dfa667f3a79842aeca2ec55e8a0526225d6d3
Dmitry Dohryk <isotop1995@gmail.com>
2019-12-28 12:36:44 +0200

    first commit

---
+++ src/File/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Header = () => {
+  return ( 
+    
+    <header className="header">
+    <img src="https://logomaster.ai/static/media/gallery002.936afb9d.png" alt=""/>
+      </header>
+  );
+}
+
+export default Header;
+
